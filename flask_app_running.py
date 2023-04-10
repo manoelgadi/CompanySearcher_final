9b9f667559bb40965735eabab040bd243ad79107
Prof. Manoel Gadi <mfalonso@faculty.ie.edu>
2023-04-10 16:26:09 +0200

    Add files via upload

---
+++ flask_app_running.py
@@ -0,0 +1,94 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask,render_template, request
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+
+import sqlite3
+conn = sqlite3.connect('./database/company_balancesheet_database.db')
+
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+df = pd.read_sql("""SELECT * FROM cuentas_anuales;""", conn)
+
+df.drop('id',inplace=True,axis=1)
+
+df.columns = ['NIF', 'Name', 'CNAE', 'Total Assets - 2017', 'Total Assets - 2016', 'Total Assets - 2015', 'Own Resources - 2017', 'Own Resources - 2016', 'Own Resources - 2015', 'Short Debt - 2017', 'Short Debt - 2016', 'Short Debt - 2015', 'Long Debt - 2017', 'Long Debt - 2016', 'Long Debt - 2015', 'Income - 2017', 'Income - 2016', 'Income - 2015', 'Amortization - 2017', 'Amortization - 2014', 'Amortization - 2015', 'Profit - 2017', 'Profit - 2016', 'Profit - 2015', 'Status']
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", df = df, ids = list(range(len(df))))
+
+
+from joblib import load
+model = load('Rating_RandomForestClassifier.joblib')
+
+@app.route("/probdefault")
+def probdefault():
+    """ Probability of Default Harded Coded """
+    # dictionary with list object in values
+    data = {
+        'ebitda_income' : [0.3],
+        'debt_ebitda' : [20],
+        'rraa_rrpp' : [0.5],
+        'log_operating_income' : [2000]
+    }
+
+    # creating a Dataframe object
+    data_df = pd.DataFrame(data)
+    X = data_df[['ebitda_income','debt_ebitda','rraa_rrpp','log_operating_income']]
+
+    # predict
+    prob_default = model.predict_proba(X)[:,1]
+
+    return "Probability of default: {}".format(prob_default)
+
+@app.route("/probdefaultform")
+def probdefaultorm():
+
+    # dictionary with list object in values
+    data = {
+        'ebitda_income' : [request.args["ebitda_income"]],
+        'debt_ebitda' : [request.args["debt_ebitda"]],
+        'rraa_rrpp' : [request.args["rraa_rrpp"]],
+        'log_operating_income' : [request.args["log_operating_income"]]
+    }
+
+    # creating a Dataframe object
+    data_df = pd.DataFrame(data).fillna(0)
+    X = data_df[['ebitda_income','debt_ebitda','rraa_rrpp','log_operating_income']]
+
+    # predict
+    probability_default = model.predict_proba(X)[:,1]
+
+    return render_template("index.html", probability_default = probability_default, df = df, ids = list(range(len(df))),)
+
+@app.route("/cs")
+def cs():
+    """ Company Searcher """
+    data=df[df['NIF']==request.args['nif']].head(1)
+    if 'model' in request.args:
+        # df['ebitda_income'] = (df.p49100_Profit_h1+df.p40800_Amortization_h1)/(df.p40100_40500_SalesTurnover_h1)        data['ebitda_income']=(data['Profit - 2017']+data['Amortization - 2017'])/data['Income - 2017']
+        # df['debt_ebitda'] =(df.p31200_ShortTermDebt_h1 + df.p32300_LongTermDebt_h1) /(df.p49100_Profit_h1+df.p40800_Amortization_h1)
+        data['debt_ebitda']=(data['Short Debt - 2017']+data['Long Debt - 2017'])/(data['Profit - 2017']+data['Amortization - 2017'])
+        # df['rraa_rrpp'] = (df.p10000_TotalAssets_h1 - df.p20000_OwnCapital_h1) /df.p20000_OwnCapital_h1
+        data['rraa_rrpp']=(data['Total Assets - 2017']-data['Own Resources - 2017'])/data['Own Resources - 2017']
+        # df['log_operating_income'] = np.log(df.p40100_40500_SalesTurnover_h1)
+        data['log_operating_income']=np.log(data['Income - 2017'])
+        data=data.replace([np.inf, -np.inf], np.nan).fillna(0)
+        X = data[['ebitda_income','debt_ebitda','rraa_rrpp','log_operating_income']]
+        data['probabilidad_default']=model.predict_proba(X)[:,1]
+        #probabilidad_default = model.predict_proba(X)[:,1]
+
+
+    if 'JSON' in request.args:
+        return data.to_json(orient='records')
+
+    else:
+        return render_template("index.html", df = df, ids = list(range(len(df))), result = 1, data = data.T.to_html())
+
+if __name__ == '__main__': # This only runs if 
+  app.run()
